b206287c6eac010c40f2880a0c470013b7772a0a
Marat Gabitov <2links@gmail.com>
2020-10-10 14:00:03 +0300

    Homework 10

--- src/game.js
+++ src/game.js
@@ -1,4 +1,73 @@
-import {showButtons} from "./main.js";
+import {countBtn, generateLog, random} from "./utils";
+import Pokemon from "./pokemon";
+import {$getElById, $control, game} from "./main";
+
+function play2() {
+    let audio = new Audio('./assets/mk3-00225.mp3');
+    audio.play();
+}
+
+class Game {
+    getPokemons = async () => {
+        const apiResponse = await fetch('https://reactmarathon-api.netlify.app/api/pokemons');
+        return await apiResponse.json();
+    }
+
+    getFightDamage = async (id_player1, id_player, id_attack) => {
+        const fightApiResponse = await fetch(`https://reactmarathon-api.netlify.app/api/fight?player1id=${id_player1}&attackId=${id_attack}&player2id=${id_player}`);
+        return await fightApiResponse.json();
+    }
+
+    start = async () => {
+        const pokemons = await this.getPokemons();
+        console.log(pokemons);
+        let i = random(pokemons.length - 1);
+        let firstPokemon = pokemons[i];
+
+        const $elImgPlayer1 = $getElById('img-player1');
+        $elImgPlayer1.src = firstPokemon.img;
+
+        let player1 = new Pokemon({
+            ...firstPokemon,
+            selectors: 'player1',
+        })
+
+        $getElById('name-player1').innerText = player1.name;
+
+        let j = random(pokemons.length - 1);
+        let secondPokemon = pokemons[j];
+
+        let player2 = new Pokemon({
+            ...secondPokemon,
+            selectors: 'player2',
+        })
+
+        $getElById('name-player2').innerText = player2.name;
+        const $elImgPlayer2 = $getElById('img-player2');
+        $elImgPlayer2.src = secondPokemon.img;
+        player1.attacks.forEach(item => {
+            const $btn = document.createElement('button');
+            $btn.classList.add('button');
+            $btn.innerText = item.name;
+            const btnCount = countBtn(item.maxCount, $btn);
+            $btn.addEventListener('click', async() => {
+                const fightDamage = await this.getFightDamage(firstPokemon.id, secondPokemon.id, item.id);
+                console.log(fightDamage);
+                btnCount();
+                play2();
+                player1.changeHP($btn, fightDamage.kick.player1, function (count) {
+                    console.log('Some change after change HP', count);
+                    generateLog(player1, player2, count);
+                });
+                player2.changeHP($btn, fightDamage.kick.player2, function (count) {
+                    console.log('Some change after change HP', count);
+                    generateLog(player2, player1, count);
+                });
+            })
+            $control.appendChild($btn);
+        });
+    }
+}
 
 function resetGame() {
     const allButtons = document.querySelectorAll('.control .button');
@@ -6,14 +75,16 @@ function resetGame() {
     const $control = document.querySelector('.control');
     const $resetBtn = document.createElement('button');
     $resetBtn.classList.add('button');
-    $resetBtn.innerText = 'Reset';
+    $resetBtn.innerText = 'Reset Game';
     $control.appendChild($resetBtn);
     $resetBtn.addEventListener("click", () => {
         const $divLogs = document.querySelector('#logs');
         $divLogs.innerHTML = '';
-        showButtons();
+        game.start();
         $resetBtn.remove();
+        $control.innerHTML = '';
     })
 }
 
 export {resetGame};
+export default Game;
--- src/main.js
+++ src/main.js
@@ -1,71 +1,21 @@
-import Pokemon from "./pokemon.js";
-import {random, generateLog, countBtn} from "./utils.js";
-import {pokemons} from "./pokemons.js";
+import Game from "./game.js";
+const game = new Game();
 
 function $getElById(id) {
     return document.getElementById(id);
 }
-function play2() {
-    let audio = new Audio('./assets/mk3-00225.mp3');
-    audio.play();
-}
+
 const $control = document.querySelector('.control');
 const $startBtn = document.createElement('button');
 $startBtn.classList.add('button');
-$startBtn.innerText = 'Start the Game!';
+$startBtn.innerText = 'Start Game!';
 $control.appendChild($startBtn);
 
 $startBtn.addEventListener("click", () => {
-    showButtons();
+    game.start();
     $startBtn.remove();
 })
 
-function showButtons() {
-    let i = random(pokemons.length - 1);
-    let firstPokemon = pokemons[i];
-
-    const $elImgPlayer1 = $getElById('img-player1');
-    $elImgPlayer1.src = firstPokemon.img;
-
-    let player1 = new Pokemon({
-        ...firstPokemon,
-        selectors: 'player1',
-    })
-
-    $getElById('name-player1').innerText = player1.name;
-
-    let j = random(pokemons.length - 1);
-    let secondPokemon = pokemons[j];
-
-    let player2 = new Pokemon({
-        ...secondPokemon,
-        selectors: 'player2',
-    })
-    
-    $getElById('name-player2').innerText = player2.name;
-    const $elImgPlayer2 = $getElById('img-player2');
-    $elImgPlayer2.src = secondPokemon.img;
-    player1.attacks.forEach(item => {
-        const $btn = document.createElement('button');
-        $btn.classList.add('button');
-        $btn.innerText = item.name;
-        const btnCount = countBtn(item.maxCount, $btn);
-        $btn.addEventListener('click', () => {
-            btnCount();
-            play2();
-            player1.changeHP($btn, random(item.maxDamage, item.minDamage), function (count) {
-                console.log('Some change after change HP', count);
-                generateLog(player1, player2, count);
-            });
-            player2.changeHP($btn, random(player2.attacks[0].maxDamage, player2.attacks[0].minDamage), function (count) {
-                console.log('Some change after change HP', count);
-                generateLog(player2, player1, count);
-            });
-        })
-        $control.appendChild($btn);
-    });
-}
-
-export {showButtons, $getElById};
+export {$getElById, $control, game};
 
 
